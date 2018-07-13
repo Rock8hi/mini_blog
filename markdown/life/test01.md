77527169d53b15cf19fd25df63420edb4fc6ff86
root <root@vultr.guest>
2018-07-13 23:18:40 +0800

    first time commit it

---
+++ markdown/life/test01.md
@@ -0,0 +1,14 @@
+<!-- title:{标题测试}} -->
+<!-- label:{测试} -->
+<!-- date:{2018/7/13} -->
+<!-- desc:{这是一个测试markdown，保存在生活分类中。mini_blog是一个个人微型博客，主要用flask开发。编辑博客用markdown，通过svn上传到服务器。服务器上通过定时服务拉取markdown svn。最后用Python markdown库转换为html格式。} -->
+
+##这里是测试测试测试
+
+这是一个测试markdown，保存在生活分类中。mini_blog是一个个人微型博客，主要用flask开发。编辑博客用markdown，通过svn上传到服务器。服务器上通过定时服务拉取markdown svn。最后用Python markdown库转换为html格式。
+
+这是一个测试markdown，保存在生活分类中。mini_blog是一个个人微型博客，主要用flask开发。编辑博客用markdown，通过svn上传到服务器。服务器上通过定时服务拉取markdown svn。最后用Python markdown库转换为html格式。
+
+这是一个测试markdown，保存在生活分类中。mini_blog是一个个人微型博客，主要用flask开发。编辑博客用markdown，通过svn上传到服务器。服务器上通过定时服务拉取markdown svn。最后用Python markdown库转换为html格式。
+
+2018年7月13日中午，星期五
---
+++ task.sh
@@ -0,0 +1,19 @@
+echo ======== start ========
+echo date: $(date "+%Y-%m-%d %H:%M")
+path=$(cd `dirname $0`; pwd)
+cd /root/blog/flask
+cd markdown
+local_revision=`svn info --password 123 | grep "Last Changed Rev:" | awk '{print $4}'`
+echo "the revision is $local_revision"
+url=`svn info --password 123 | grep URL: | awk '{print $2}'`
+remote_revision=`svn info $url --password 123 | grep "Last Changed Rev:" | awk '{print $4}'`
+echo "the revision in svn is $remote_revision"
+if [[ $local_revision < $remote_revision ]] ; then
+    echo "need svn update"
+    svn update --password 123
+    echo "svn update finish"
+else
+    echo "not need svn update"
+fi
+cd $path
+echo ========= end =========
